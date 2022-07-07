b4a3e3af0439b2bfbd8ea064fc7d0812e205c64b
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-07-07 17:23:07 -0400

    Done with moch challeng

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-// write your code here
+const hamsterMenu = document.getElementById("hamster-menu");
+const detailImage = document.querySelector(".detail-image")
+const hamsterName = document.querySelector(".name")
+const hamsterOccupation = document.querySelector(".occupation")
+
+
+
+fetch("http://localhost:3000/hamsters")
+.then(resp => resp.json())
+.then(data => data.forEach(hamsterImages))
+
+function hamsterImages(hamsters){
+    const imgElement = document.createElement("img")
+    imgElement.src = hamsters.image
+    hamsterMenu.append(imgElement)
+
+imgElement.addEventListener("click", () => {
+detailImage.src = hamsters.image;
+hamsterName.innerText = hamsters.name;
+hamsterOccupation.innerText = hamsters.occupation;
+})
+}
+
+const form = document.getElementById("new-hamster");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+
+    const newName = document.getElementById("new-name").value
+    const newOccupation = document.getElementById("new-occupation").value
+    const newImage = document.getElementById("new-image").value
+
+   const hamsters = {
+       name: newName,
+       occupation: newOccupation,
+       image: newImage
+   }
+
+hamsterImages(hamsters);
+form.reset();
+})
